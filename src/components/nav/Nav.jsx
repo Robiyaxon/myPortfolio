06e03d7eba59a7aef77ed7dec3a04ecf8a90ca17
Robiyaxon <tuhtaevarobiahon@gmail.com>
2023-01-17 22:07:04 +0500

    pull

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable eqeqeq */
+
 import { Component } from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
@@ -19,35 +19,35 @@ class Nav extends Component {
       <nav>
         <a
           onClick={() => this.setState({ active: "#" })}
-          className={this.state.active == "#" ? "active" : ""}
+          className={this.state.active === "#" ? "active" : ""}
           href="#"
         >
           <AiOutlineHome />
         </a>
         <a
           onClick={() => this.setState({ active: "#about" })}
-          className={this.state.active == "#about" ? "active" : ""}
+          className={this.state.active === "#about" ? "active" : ""}
           href="#about"
         >
           <AiOutlineUser />
         </a>
         <a
           onClick={() => this.setState({ active: "#experience" })}
-          className={this.state.active == "#experience" ? "active" : ""}
+          className={this.state.active === "#experience" ? "active" : ""}
           href="#experience"
         >
           <BiBook />
         </a>
         <a
           onClick={() => this.setState({ active: "#services" })}
-          className={this.state.active == "#services" ? "active" : ""}
+          className={this.state.active === "#services" ? "active" : ""}
           href="#services"
         >
           <RiServiceLine />
         </a>
         <a
           onClick={() => this.setState({ active: "#contact" })}
-          className={this.state.active == "#contact" ? "active" : ""}
+          className={this.state.active === "#contact" ? "active" : ""}
           href="#contact"
         >
           <BiMessageSquareDetail />
