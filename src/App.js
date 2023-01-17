b92c510bbe4b34615b563435af10e2b9bd0fd1b0
Robiyaxon <tuhtaevarobiahon@gmail.com>
2023-01-17 22:14:13 +0500

    pull

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         <Experience />
         <Services />
         <Portfolio />
+        <Contact />
         <Footer />
       </>
     );
